dd8d8752aa6ffef4848a8c46c5aa34298c577346
da-the-dev <leshiktk@me.com>
2023-07-29 03:17:39 +0300

    Added name loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
-<script>
+<script lang="ts">
 	import vk from '$lib/shared/stores/vk'
 	import Vk from '$lib/icons/vk.svelte'
+	import { get } from 'svelte/store'
+
+	async function getName(): Promise<string> {
+		const res = await fetch('https://api.vk.com/method/users.get', {
+			method: 'POST',
+			body: JSON.stringify({
+				user_ids: get(vk).user_id,
+				fields: 'about',
+				access_token: get(vk).access_token,
+			}),
+		})
+		const json = await res.json()
+		return json['first_name'] + json['last_name']
+	}
 </script>
 
 <svelte:head>
@@ -9,12 +23,23 @@
 </svelte:head>
 
 <section>
-	{#if $vk.user_id != -1}
-		<p>{$vk.user_id}</p>
-	{/if}
+	{#await getName()}
+		<p>Loading</p>
+	{:then name}
+		<p>{name}</p>
+	{:catch}
+		<p>Failed to load name</p>
+	{/await}
+
 	<div class="auth-b">
-		<button class="vk-auth-button" on:click={() => window.location.replace('https://oauth.vk.com/authorize?client_id=51717321&redirect_uri=https://open-api-task.vercel.app/verify&scope=12&display=page&response_type=token')}>
-			<Vk/>
+		<button
+			class="vk-auth-button"
+			on:click={() =>
+				window.location.replace(
+					'https://oauth.vk.com/authorize?client_id=51717321&redirect_uri=https://open-api-task.vercel.app/verify&scope=12&display=page&response_type=token'
+				)}
+		>
+			<Vk />
 		</button>
 	</div>
 </section>
@@ -33,11 +58,10 @@
 	}
 
 	.vk-auth-button {
-		width : 4rem;
+		width: 4rem;
 		height: 4rem;
 		border: none;
 		border-radius: 10px;
 		background-color: var(--color-bg-2);
 	}
-
 </style>
